36a1f7afdc2a486974b105652811f9a124d15fa4
nh-g <giangngohong@gmail.com>
2021-07-21 11:37:47 +0200

    display added todo to the todolist

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 export default function TodoList() {
@@ -10,15 +11,26 @@ export default function TodoList() {
             return
         }
         const newTodos = [todo, ...todos]
-
         setTodos(newTodos)
-        console.log(todo, ...todos);
     }
+
+    const completeTodo = id => {
+        let updatedTodos = todos.map(todo => {
+            if(todo.id === id) {
+                todo.isComplete = !todo.isComplete;
+            }
+            return todo;
+        })
+        setTodos(updatedTodos)
+    }
+
+
 
     return (
         <div>
             <h1>What's the plan for Today?</h1>
             <TodoForm onSubmit={addTodo}/>
+            <Todo todos ={todos} completeTodo ={completeTodo}/>
         </div>
     )
 }
